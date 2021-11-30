064ec4e50546fff932b1f9051c05149d1397707e
Hangloose <85841690+Esoolgnah@users.noreply.github.com>
2021-11-30 23:48:58 +0900

    블로그만들기 7화

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
+/* eslint-disable */
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+  let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동 맛집','파이썬 독학']);
+  let [따봉, 따봉변경] = useState();
+
+  // function 글순서바꾸기(){
+  //   let newArray = [...글제목];
+  //   newArray.sort();
+  //   글제목변경(newArray);
+  // }
+
   return (
     <div className="App">
-      <div className = "black-nav">
-        개발 Blog
+      <div className="black-nav">
+        <div>개발 Blog</div>
       </div>
+      <div className="list">
+        <h3> { 글제목[0]} <span onClick={ ()=> { 따봉변경( 따봉 + 1) }}>👍🏻</span> {따봉}</h3>
+        <p>2월 16일 발행</p>
+        <hr/>
+        <h3> { 글제목[1]} </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+        <h3> { 글제목[2]} </h3>
+        <p>2월 18일 발행</p>  
+        <hr/>
+      </div>
+
+     <Modal />
+     
     </div>
   );
 }
 
+function Modal(){
+  return(
+      <div className="modal">
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+  )
+}
 export default App;

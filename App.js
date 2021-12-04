7fc309f44373829be13192a3297ad2119db54b25
Hangloose <85841690+Esoolgnah@users.noreply.github.com>
2021-12-04 17:18:04 +0900

    8강 완료

--- App.js
+++ App.js
@@ -1,36 +1,43 @@
-/* eslint=disable */
-
+/* eslint-disable */
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  let posts = '강남 고기 맛집';
 
-  let [글제목, 글제목변경] = useState(['남자코트 추천','강남 우동맛집','파이썬 독학'])
+
+function App() {
+  let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동 맛집','파이썬 독학']);
+  let [따봉, 따봉변경] = useState(0);
+  let [modal, modal변경] = useState(false);
 
   return (
     <div className="App">
-      <div className = "black-nav">
+      <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-    <div className = "list">
-      <h3> { 글제목[0] } <span onClick={ ()=> {console.log(1)} }>👍🏻</span></h3>
-      <p>10월 04일 발행</p>
-      <hr/>
-    </div>
-    <div className = "list">
-      <h3> { 글제목[1] }</h3>
-      <p>10월 05일 발행</p>
-      <hr/>
-    </div>
-    <div className = "list">
-      <h3> { 글제목[2] }</h3>
-      <p>10월 06일 발행</p>
-      <hr/>
-    </div>
+
+    {
+      글제목.map(function(글){
+        return (
+        <div className="list">
+          <h3>{글}<span onClick={ ()=> { 따봉변경( 따봉 + 1) }}>👍🏻</span> {따봉}</h3>
+          <p>2월 18일 발행</p>
+          <hr/>
+        </div>
+        )
+      })
+    }
     </div>
   );
 }
 
+function Modal(){
+  return(
+      <div className="modal">
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+  )
+}
 export default App;

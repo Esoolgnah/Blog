5d802d1e9a0613a14a0ce7a219909303a642f9d4
Hangloose <85841690+Esoolgnah@users.noreply.github.com>
2021-12-05 23:52:43 +0900

    9강

--- src/App.js
+++ src/App.js
@@ -5,39 +5,23 @@ import './App.css';
 
 
 
-function App() {
-  let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동 맛집','파이썬 독학']);
-  let [따봉, 따봉변경] = useState(0);
-  let [modal, modal변경] = useState(false);
-
+function App (){
+  let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
   return (
-    <div className="App">
-      <div className="black-nav">
-        <div>개발 Blog</div>
-      </div>
-
-    {
-      글제목.map(function(글){
-        return (
-        <div className="list">
-          <h3>{글}<span onClick={ ()=> { 따봉변경( 따봉 + 1) }}>👍🏻</span> {따봉}</h3>
-          <p>2월 18일 발행</p>
-          <hr/>
-        </div>
-        )
-      })
-    }
+    <div>
+      ...
+      <Modal 글제목={글제목}></Modal>
     </div>
-  );
+  )
 }
 
-function Modal(){
-  return(
-      <div className="modal">
-        <h2>제목</h2>
-        <p>날짜</p>
-        <p>상세내용</p>
-      </div>
+function Modal(props){
+  return (
+    <div className="modal">
+      <h2>제목 { props.글제목[0] }</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
   )
 }
 export default App;

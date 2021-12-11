8c8d1e24c57eb863d54cdae0832fcc93a7f61b89
Hangloose <85841690+Esoolgnah@users.noreply.github.com>
2021-12-11 14:45:05 +0900

    11강 완료

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 /* eslint-disable */
-import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import react, { useState } from 'react';
 
 function App() {
-  let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동 맛집','파이썬 독학']);
+  let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동 맛집', '파이썬 독학']);
   let [따봉, 따봉변경] = useState(0);
   let [modal, modal변경] = useState(false);
   let [누른제목, 누른제목변경] = useState(0);
+
+  let [입력값, 입력값변경] = useState('');
 
   return (
     <div className="App">
@@ -16,34 +16,37 @@ function App() {
         <div>개발 Blog</div>
       </div>
       {
-        글제목.map(function(글,i){
-        return (
-          <div className="list">
-            <h3 onClick = { () => 누른제목변경(i) } >{글} <span onClick = {() =>{따봉변경(따봉+1)}}>👍🏻</span>
-            {따봉}</h3>
-             <p>2월 18일 발행</p>
-           <hr/>
-          </div>
-        )
-      })
-    }
-    <button onClick={() => {modal변경(modal===false ? modal = true : modal = false)}} >열고 닫기</button>
-    {
-    modal === true
-    ?< Modal 글제목={글제목} 누른제목 = {누른제목}></Modal>
-    :null
-    }
-    </div>
-  );
-}
+        글제목.map((글, idx) =>{
+          return(
+            <div className="list" key={idx}>
+              <h3 onClick = {() => {누른제목변경(idx)}}>{글}<span onClick = { () => {따봉변경(따봉+1)} }>👍🏻
+              </span>{따봉}</h3>
+              <p>2월 18일 발행</p>
+              <hr/>
+            </div>
 
-function Modal(props){
-  return(
-      <div className="modal">
-        <h2>{props.글제목[props.누른제목]}</h2>
-        <p>날짜</p>
-        <p>상세내용</p>
-      </div>
+          )
+        })
+      }
+      {/* <input onChange = { (e) => {입력값변경(e.target.value) } }></input> */}
+
+      <button onClick = {() => {modal변경(!modal)}}>열고 닫기</button>
+      {
+        modal === true
+        ?<Modal 글제목 = {글제목} 누른제목 = {누른제목}></Modal>
+        :null
+      }
+    </div>
   )
 }
+function Modal(props){
+  return (
+    <div className = "modal">
+      <h2>{props.글제목[props.누른제목]}</h2>
+      <p>날짜</p>
+      <p>상세 내용</p>
+    </div>
+  )
+}
+
 export default App;

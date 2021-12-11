7cc51e65755371dabd6980eeb46fe99d261d52c0
Hangloose <85841690+Esoolgnah@users.noreply.github.com>
2021-12-11 13:23:21 +0900

    10강 완료

--- src/App.js
+++ src/App.js
@@ -4,41 +4,33 @@ import logo from './logo.svg';
 import './App.css';
 
 
-
 function App() {
   let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동 맛집','파이썬 독학']);
-  let [따봉, 따봉변경] = useState();
-
+  let [따봉, 따봉변경] = useState(0);
   let [modal, modal변경] = useState(false);
-
-  // function 글순서바꾸기(){
-  //   let newArray = [...글제목];
-  //   newArray.sort();
-  //   글제목변경(newArray);
-  // }
+  let [누른제목, 누른제목변경] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <div className="list">
-        <h3> { 글제목[0]} <span onClick={ ()=> { 따봉변경( 따봉 + 1) }}>👍🏻</span> {따봉}</h3>
-        <p>2월 16일 발행</p>
-        <hr/>
-        <h3 > { 글제목[1]} </h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-        <h3> { 글제목[2]} </h3>
-        <p>2월 18일 발행</p>  
-        <hr/>
-      </div>
-
-     
+      {
+        글제목.map(function(글,i){
+        return (
+          <div className="list">
+            <h3 onClick = { () => 누른제목변경(i) } >{글} <span onClick = {() =>{따봉변경(따봉+1)}}>👍🏻</span>
+            {따봉}</h3>
+             <p>2월 18일 발행</p>
+           <hr/>
+          </div>
+        )
+      })
+    }
     <button onClick={() => {modal변경(modal===false ? modal = true : modal = false)}} >열고 닫기</button>
     {
     modal === true
-    ?< Modal 글제목={글제목} ></Modal>
+    ?< Modal 글제목={글제목} 누른제목 = {누른제목}></Modal>
     :null
     }
     </div>
@@ -48,7 +40,7 @@ function App() {
 function Modal(props){
   return(
       <div className="modal">
-        <h2>{props.글제목[0]}</h2>
+        <h2>{props.글제목[props.누른제목]}</h2>
         <p>날짜</p>
         <p>상세내용</p>
       </div>
